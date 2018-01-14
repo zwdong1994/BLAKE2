aaeb7db2a936826716ac6b5f5b51d5ae4c65d9d5
victor <zwdong1994@gmail.com>
2018-01-14 16:40:23 +0800

    Simple comparision between BLAKE2b and traditional hash algorithm(md5 sha1 sha256).

--- sse/simple_blake2b.cpp
+++ sse/simple_blake2b.cpp
@@ -10,26 +10,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
 #include <string.h>
 
 
 int main(){
-	uint8_t buf[4096];
-	size_t  out_length = 16;
-	uint8_t key[BLAKE2B_KEYBYTES];
+    size_t buf_length = 32;
+	uint8_t buf[buf_length];
+	size_t  out_length = 64;
 	uint8_t  hash[out_length];
 	double stat_time = 0.0;
 	double end_time = 0.0;
-	for( int i = 0; i < BLAKE2B_KEYBYTES; ++i )
-		key[i] = ( uint8_t )i;
-	for( int i = 0; i < 4096; ++i ){
+
+    memset(hash, 0, out_length);
+	for( int i = 0; i < buf_length; ++i ){
 		buf[i] = 'a' + i % 26;
 	}
 	stat_time = get_time();
-	blake2b( hash, out_length, buf, 4096, key, BLAKE2B_KEYBYTES );
+	blake2b( hash, out_length -1, buf, buf_length, NULL, 0 );
 	end_time = get_time();
-	printf("%lf\n", (end_time -stat_time) * 1000);
-	printf("%s\n", hash);
+
+
+    printf("blake2b:\t%lf\n", (end_time -stat_time) * 1000);
+    memset(hash, 0, out_length);
+    //printf("%s\n", hash);
+    stat_time = get_time();
+    MD5((unsigned char *)buf, (size_t)buf_length, (unsigned char *)hash);
+
+    end_time = get_time();
+    printf("    md5:\t%lf\n", (end_time -stat_time) * 1000);
+    memset(hash, 0, out_length);
+    stat_time = get_time();
+    SHA256((unsigned char *)buf, (size_t)buf_length, (unsigned char *)hash);
+
+    end_time = get_time();
+    printf(" sha256:\t%lf\n", (end_time -stat_time) * 1000);
+    memset(hash, 0, out_length);
+    stat_time = get_time();
+    SHA1((unsigned char *)buf, (size_t)buf_length, (unsigned char *)hash);
+    memset(hash, 0, out_length);
+    end_time = get_time();
+    printf("   sha1:\t%lf\n", (end_time -stat_time) * 1000);
 
 	return 1;
 }
